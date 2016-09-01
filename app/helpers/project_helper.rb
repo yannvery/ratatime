44aae06d5f022dcf7ffe87429f98423ab2edfafb
Yann VERY <yann.very@gmail.com>
2016-09-01 11:38:02 +0200

    Fix chart on dashboard

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -7,4 +7,8 @@ module ProjectHelper
   def project_chart_color(user, date)
     ProjectChart.new(user: user).by_date(date).values.map { |p| p[:color] || 'grey'}
   end
+
+  def project_chart_label(user, date)
+    ProjectChart.new(user: user).by_date(date).values.map { |p| ''}
+  end
 end
